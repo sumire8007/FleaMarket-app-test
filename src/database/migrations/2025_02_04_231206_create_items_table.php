ea382074c35121527ea3a887b529be27bff798cd
sumire sato <sumire.8007@gmail.com>
2025-02-06 00:52:04 +0900

    view・CSSファイルの作成

---
+++ src/database/migrations/2025_02_04_231206_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('user_id')->constrained();
+            $table->foreignID('category_id')->constrained();
+            $table->foreignID('condition_id')->constrained();
+            $table->string('item_name');
+            $table->integer('price');
+            $table->text('detail');
+            $table->string('item_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
--- src/database/migrations/2025_02_04_231307_create_addresses_table.php
+++ src/database/migrations/2025_02_04_231307_create_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignID('user_id')->constrained()->cascadeOnDelete();
+            $table->string('user_img');
+            $table->string('post_code');
+            $table->string('address');
+            $table->string('building');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 }
